9601b0b8585647230f2682901db56e8e158f398c
Arjuna Kristophe Sankar <33190221+kristophesankar@users.noreply.github.com>
2018-12-11 12:54:22 -0400

    Remove Flipbook

--- js/home.js
+++ js/home.js
@@ -3,7 +3,6 @@ var Home = function() {
     this.main = document.getElementById("navbar");
     this.sticky = this.getScreenHeight();
     this.initSlick();
-    this.initFlipbook();
     this.initSlide();
 };
 
@@ -109,95 +108,4 @@ Home.prototype.initSlick = function() {
 
 };
 
-Home.prototype.initFlipbook = function() {
-    window.jQuery(function() {
-        var $ = window.jQuery;
-        var styleClb = function() {
-                $('.fb3d-modal').removeClass('light').addClass('dark');
-            },
-            booksOptions = {
 
-                book1: {
-                    pdf: './magazines/august_2018_magazine.pdf',
-                    propertiesCallback: function(props) {
-                        props.page.depth /= 3;
-                        return props;
-                    },
-                    template: {
-                        html: './templates/default-book-view.html',
-                        styles: [
-                            './css/black-book-view.css'
-                        ],
-                        links: [{
-                            rel: 'stylesheet',
-                            href: './css/font-awesome.min.css'
-                        }],
-                        script: './js/default-book-view.js',
-                        sounds: {
-                            startFlip: './sounds/start-flip.mp3',
-                            endFlip: './sounds/end-flip.mp3'
-                        }
-                    },
-                    styleClb: styleClb
-                },
-                book2: {
-                    pdf: './magazines/September_2018_Magazine_Final.pdf',
-                    propertiesCallback: function(props) {
-                        props.page.depth /= 3;
-                        return props;
-                    },
-                    template: {
-                        html: './templates/default-book-view.html',
-                        styles: [
-                            './css/black-book-view.css'
-                        ],
-                        links: [{
-                            rel: 'stylesheet',
-                            href: './css/font-awesome.min.css'
-                        }],
-                        script: './js/default-book-view.js',
-                        sounds: {
-                            startFlip: './sounds/start-flip.mp3',
-                            endFlip: './sounds/end-flip.mp3'
-                        }
-                    },
-                    styleClb: styleClb
-                }
-            };
-
-        var instance = {
-            scene: undefined,
-            options: undefined,
-            node: $('.fb3d-modal .mount-container')
-        };
-
-        var modal = $('.fb3d-modal');
-        modal.on('fb3d.modal.hide', function() {
-            instance.scene.dispose();
-        });
-        modal.on('fb3d.modal.show', function() {
-            instance.scene = instance.node.FlipBook(instance.options);
-            instance.options.styleClb();
-        });
-        $('.books').find('.thumbnail').click(function(e) {
-            var target = $(e.target);
-            while (target[0] && !target.attr('data-book-id')) {
-                target = $(target[0].parentNode);
-            }
-            if (target[0]) {
-                instance.options = booksOptions[target.attr('data-book-id')];
-                $('.fb3d-modal').fb3dModal('show');
-            }
-        });
-        $('.books').find('.btn').click(function(e) {
-            var target = $(e.target);
-            while (target[0] && !target.attr('data-book-id')) {
-                target = $(target[0].parentNode);
-            }
-            if (target[0]) {
-                instance.options = booksOptions[target.attr('data-book-id')];
-                $('.fb3d-modal').fb3dModal('show');
-            }
-        });
-    });
-};
