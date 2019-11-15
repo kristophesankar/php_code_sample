2eb998897318265eadbe15d216e7b5372b000268
Arjuna Kristophe Sankar <33190221+kristophesankar@users.noreply.github.com>
2019-11-14 23:19:23 -0400

    feat: updated to recent

--- includes/home-slider.php
+++ includes/home-slider.php
@@ -1,64 +1,233 @@
-<?php
+<div id="header-custom" class="slickitems slick-slider">
 
-echo "
-
-<div id=\"header-custom\" class=\"slickitems slick-slider\">
-
-  <div class=\"slick-slide\" style=\"height: 90vh;position: relative;background: #2a2f27 url(./images/home-slider/10.jpg) no-repeat;background-size: cover;\">
-    <div class=\"row img-header-row\">
-      <div class=\"col-md-2\"></div>
-      <div class=\"col-md-8\">
-        <a href=\"index.php\"><img style=\"width:70%;height:auto;display: block;margin-left: auto;margin-right: auto;\" class=\"img-responsive col-lg-12 imglogo\" src=\"./images/home-slider/ctcwhite.png\" alt=\"\"></a>
-        <div class=\"header-sub\">
-          <h2 class=\"contentthemetitle\"></h2>
-          <p class=\"contentthemedescription\"></p>
+  <div class="slick-slide"
+    style="
+      height: 90vh;
+      position: relative;
+      background: #2a2f27 url(./images/home-slider/16.jpg) no-repeat;
+      background-size: cover;">
+    <div class="row img-header-row justify-content-center">
+      <div class="col-md-8">
+        <a href="index.php">
+          <img
+            style="
+              width:70%;
+              height:auto;
+              display: block;
+              margin-left: auto;
+              margin-right: auto;"
+            class="img-responsive col-lg-12 imglogo"
+            src="./images/home-slider/ctcwhite.png" alt="">
+        </a>
+        <div class="header-sub">
+          <h2 class="contentthemetitle"></h2>
+          <p class="contentthemedescription"></p>
         </div>
       </div>
     </div>
   </div>
 
-  <div id=\"\" class=\"container-fluid slick-slide\" style=\"height: 90vh;position: relative;background: #2a2f27 url(./images/home-slider/9.jpg) no-repeat;background-size: cover;\">
-    <div class=\"row img-header-row\">
-      <div class=\"col-lg-2\"></div>
-      <div class=\"col-md-12 col-lg-8\">
-        <a href=\"index.php\"><img style=\"width:70%;height:auto;display: block;margin-left: auto;margin-right: auto;\" class=\"img-responsive col-lg-12 imglogo\" src=\"./images/home-slider/ctcwhite.png\" alt=\"\"></a>
-        <div class=\"header-sub\">
-        <h2 class=\"contentthemetitle\"></h2>
-        <p class=\"contentthemedescription\"></p>
+  <div id="" class="container-fluid slick-slide" style="height: 90vh;position: relative;background: #2a2f27 url(./images/home-slider/17.jpg) no-repeat;background-size: cover;">
+    <div class="row img-header-row justify-content-center">
+      <div class="col-md-12 col-lg-8">
+        <a href="index.php"><img style="width:70%;height:auto;display: block;margin-left: auto;margin-right: auto;" class="img-responsive col-lg-12 imglogo" src="./images/home-slider/ctcwhite.png" alt=""></a>
+        <div class="header-sub">
+        <h2 class="contentthemetitle"></h2>
+        <p class="contentthemedescription"></p>
         </div>
       </div>
     </div>
   </div>
 
-  <div id=\"\" class=\"container-fluid slick-slide\" style=\"height: 90vh;position: relative;background: #2a2f27 url(./images/home-slider/8.jpg) no-repeat;background-size: cover;\">
-    <div class=\"row img-header-row\">
-      <div class=\"col-lg-2\"></div>
-      <div class=\"col-lg-8\">
-        <a href=\"index.php\"><img style=\"width:70%;height:auto;display: block;margin-left: auto;margin-right: auto;\" class=\"img-responsive col-lg-12 imglogo\" src=\"./images/home-slider/ctccafeds.png\" alt=\"\"></a>
-        <div class=\"header-sub\">
-        <h2 class=\"contentthemetitle\"></h2>
-        <p class=\"contentthemedescription\"></p>
+  <div id="" class="container-fluid slick-slide" style="height: 90vh;position: relative;background: #2a2f27 url(./images/home-slider/18.jpg) no-repeat;background-size: cover;">
+    <div class="row img-header-row justify-content-center">
+      <div class="col-lg-8">
+        <a href="index.php"><img style="width:70%;height:auto;display: block;margin-left: auto;margin-right: auto;" class="img-responsive col-lg-12 imglogo" src="./images/home-slider/ctcwhite.png" alt=""></a>
+        <div class="header-sub">
+        <h2 class="contentthemetitle"></h2>
+        <p class="contentthemedescription"></p>
         </div>
       </div>
     </div>
   </div>
 
-  <div id=\"\" class=\"container-fluid slick-slide\" style=\"height: 90vh;position: relative;background: #2a2f27 url(./images/home-slider/7.jpg) no-repeat;background-size: cover;\">
-    <div class=\"row img-header-row\">
-      <div class=\"col-lg-2\"></div>
-      <div class=\"col-lg-8\">
-        <a href=\"index.php\"><img style=\"width:70%;height:auto;display: block;margin-left: auto;margin-right: auto;\" class=\"img-responsive col-lg-12 imglogo\" src=\"./images/home-slider/ctccafeds.png\" alt=\"\"></a>
-        <div class=\"header-sub\">
-        <h2 class=\"contentthemetitle\"></h2>
-        <p class=\"contentthemedescription\"></p>
+  <div id="" class="container-fluid slick-slide" style="height: 90vh;position: relative;background: #2a2f27 url(./images/home-slider/19.jpg) no-repeat;background-size: cover;">
+    <div class="row img-header-row justify-content-center">
+      <div class="col-lg-8">
+        <a href="index.php"><img style="width:70%;height:auto;display: block;margin-left: auto;margin-right: auto;" class="img-responsive col-lg-12 imglogo" src="./images/home-slider/ctcwhite.png" alt=""></a>
+        <div class="header-sub">
+        <h2 class="contentthemetitle"></h2>
+        <p class="contentthemedescription"></p>
         </div>
       </div>
     </div>
   </div>
 
+
+  <div class="slick-slide"
+    style="
+      height: 90vh;
+      position: relative;
+      background: #2a2f27 url(./images/home-slider/20.jpg) no-repeat;
+      background-size: cover;">
+    <div class="row img-header-row justify-content-center">
+      <div class="col-md-8">
+        <a href="index.php">
+          <img
+            style="
+              width:70%;
+              height:auto;
+              display: block;
+              margin-left: auto;
+              margin-right: auto;"
+            class="img-responsive col-lg-12 imglogo"
+            src="./images/home-slider/ctcwhite.png" alt="">
+        </a>
+        <div class="header-sub">
+          <h2 class="contentthemetitle"></h2>
+          <p class="contentthemedescription"></p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+  <div class="slick-slide"
+    style="
+      height: 90vh;
+      position: relative;
+      background: #2a2f27 url(./images/home-slider/21.jpg) no-repeat;
+      background-size: cover;">
+    <div class="row img-header-row justify-content-center">
+      <div class="col-md-8">
+        <a href="index.php">
+          <img
+            style="
+              width:70%;
+              height:auto;
+              display: block;
+              margin-left: auto;
+              margin-right: auto;"
+            class="img-responsive col-lg-12 imglogo"
+            src="./images/home-slider/ctcwhite.png" alt="">
+        </a>
+        <div class="header-sub">
+          <h2 class="contentthemetitle"></h2>
+          <p class="contentthemedescription"></p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+  <div class="slick-slide"
+    style="
+      height: 90vh;
+      position: relative;
+      background: #2a2f27 url(./images/home-slider/23.jpg) no-repeat;
+      background-size: cover;">
+    <div class="row img-header-row justify-content-center">
+      <div class="col-md-8">
+        <a href="index.php">
+          <img
+            style="
+              width:70%;
+              height:auto;
+              display: block;
+              margin-left: auto;
+              margin-right: auto;"
+            class="img-responsive col-lg-12 imglogo"
+            src="./images/home-slider/ctcwhite.png" alt="">
+        </a>
+        <div class="header-sub">
+          <h2 class="contentthemetitle"></h2>
+          <p class="contentthemedescription"></p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+  <div class="slick-slide"
+    style="
+      height: 90vh;
+      position: relative;
+      background: #2a2f27 url(./images/home-slider/24.jpg) no-repeat;
+      background-size: cover;">
+    <div class="row img-header-row justify-content-center">
+      <div class="col-md-8">
+        <a href="index.php">
+          <img
+            style="
+              width:70%;
+              height:auto;
+              display: block;
+              margin-left: auto;
+              margin-right: auto;"
+            class="img-responsive col-lg-12 imglogo"
+            src="./images/home-slider/ctcwhite.png" alt="">
+        </a>
+        <div class="header-sub">
+          <h2 class="contentthemetitle"></h2>
+          <p class="contentthemedescription"></p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+  <div class="slick-slide"
+    style="
+      height: 90vh;
+      position: relative;
+      background: #2a2f27 url(./images/home-slider/25.jpg) no-repeat;
+      background-size: cover;">
+    <div class="row img-header-row justify-content-center">
+      <div class="col-md-8">
+        <a href="index.php">
+          <img
+            style="
+              width:70%;
+              height:auto;
+              display: block;
+              margin-left: auto;
+              margin-right: auto;"
+            class="img-responsive col-lg-12 imglogo"
+            src="./images/home-slider/ctcwhite.png" alt="">
+        </a>
+        <div class="header-sub">
+          <h2 class="contentthemetitle"></h2>
+          <p class="contentthemedescription"></p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="slick-slide"
+    style="
+      height: 90vh;
+      position: relative;
+      background: #2a2f27 url(./images/home-slider/26.jpg) no-repeat;
+      background-size: cover;">
+    <div class="row img-header-row justify-content-center">
+      <div class="col-md-8">
+        <a href="index.php">
+          <img
+            style="
+              width:70%;
+              height:auto;
+              display: block;
+              margin-left: auto;
+              margin-right: auto;"
+            class="img-responsive col-lg-12 imglogo"
+            src="./images/home-slider/ctcwhite.png" alt="">
+        </a>
+        <div class="header-sub">
+          <h2 class="contentthemetitle"></h2>
+          <p class="contentthemedescription"></p>
+        </div>
+      </div>
+    </div>
+  </div>
 
 </div>
-
-";
-
- ?>
