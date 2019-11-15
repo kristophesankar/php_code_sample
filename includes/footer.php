d13a90c4a688cb6e89285cb41407da12c586687b
Arjuna Kristophe Sankar <33190221+kristophesankar@users.noreply.github.com>
2019-11-14 23:31:31 -0400

    feat: updated to recent

--- includes/footer.php
+++ includes/footer.php
@@ -1,21 +1,25 @@
-<?php
-
-echo "
-
-<div id=\"copyright\">
-  <div class=\"text-center\">
-
-  </div>
-  <div class=\"container\" style=\"font-size: 16px;\">
-
+<div id="copyright">
+  <div class="container" style="font-size: 16px;">
     &copy; 2018 CoffeeTable Coven<br>
-    <p class=\"terms-policy\"><a href=\"terms.php#nav\" style=\"font-size: 14px; color:blue;\">Terms of Use</a>&nbsp;|&nbsp;<a href=\"privacy.php#nav\" style=\"font-size: 14px; color:blue;\">Privacy Policy</a></p>
-
-    <span style=\"font-size: 14px; \"><p class=\"terms-policy\">Site built by <a style=\"color:blue;\" href=\"http://patchdev.com\">PatchDev<a></p></span>
+    <p class="terms-policy">
+      <a href="terms.php#nav" style="font-size: 14px; color:blue;">
+        Terms of Use
+      </a>
+      &nbsp;|&nbsp;
+      <a href="privacy.php#nav" style="font-size: 14px; color:blue;">
+        Privacy Policy
+      </a>
+    </p>
+    <span style="font-size: 14px; ">
+      <p class="terms-policy">
+        Site built by
+        <a style="color:blue;"
+          href="https://www.linkedin.com/in/arjuna-kristophe-sankar-67a736109/">
+            Kristophe Sankar
+        <a>
+      </p>
+    </span>
   </div>
-
 </div>
 
-";
-
-?>
+<script src="js/services/home.js"></script>
