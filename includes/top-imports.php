c018d082754c67b1513ab3b90a868f1b8336ade9
Arjuna Kristophe Sankar <33190221+kristophesankar@users.noreply.github.com>
2019-11-14 23:18:42 -0400

    feat: updated to recent

--- includes/top-imports.php
+++ includes/top-imports.php
@@ -1,41 +1,32 @@
-<?php
-
-echo "
-
-<meta http-equiv=\"content-type\" content=\"text/html; charset=utf-8\" />
-<meta name=\"description\" content=\"\" />
-<meta name=\"keywords\" content=\"\" />
-<link href=\"https://fonts.googleapis.com/css?family=Tajawal\" rel=\"stylesheet\">
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-135441484-1"></script>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<meta name="description" content=" />
+<meta name="keywords" content=" />
+<link href="https://fonts.googleapis.com/css?family=Tajawal" rel="stylesheet">
 <link href='http://fonts.googleapis.com/css?family=Arimo:400,700' rel='stylesheet' type='text/css'>
-<link href=\"https://fonts.googleapis.com/css?family=Montserrat\" rel=\"stylesheet\">
-<link href=\"https://fonts.googleapis.com/css?family=Quicksand\" rel=\"stylesheet\">
-<!--[if lte IE 8]><script src=\"js/html5shiv.js\"></script><![endif]-->
-<script src=\"js/jquery_v1.11.0.js\"></script>
-<script src=\"js/jquery-migrate-1.2.1.min.js\"></script>
-<script src=\"js/skel.min.js\"></script>
-<script src=\"js/skel-panels.min.js\"></script>
-<script src=\"js/init.js\"></script>
-<link rel=\"stylesheet\" href=\"./node_modules/native-toast/dist/native-toast.css\">
-<script src=\"./node_modules/native-toast/dist/native-toast.js\"></script>
-<script src=\"./node_modules/bootstrap/dist/js/bootstrap.js\"></script>
-<script src=\"./node_modules/smoothscroll/smoothscroll.min.js\"></script>
+<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+<!--[if lte IE 8]><script src="js/html5shiv.js"></script><![endif]-->
+<script src="js/jquery_v1.11.0.js"></script>
+<script src="js/jquery-migrate-1.2.1.min.js"></script>
+<script src="js/skel.min.js"></script>
+<script src="js/skel-panels.min.js"></script>
+<script src="js/init.js"></script>
+<link rel="stylesheet" href="./node_modules/native-toast/dist/native-toast.css">
+<script src="./node_modules/native-toast/dist/native-toast.js"></script>
+<script src="./node_modules/bootstrap/dist/js/bootstrap.js"></script>
+<script src="./node_modules/smoothscroll/smoothscroll.min.js"></script>
 <noscript>
-    <link rel=\"stylesheet\" href=\"css/skel-noscript.css\" />
-    <link rel=\"stylesheet\" href=\"css/style.css\" />
-    <link rel=\"stylesheet\" href=\"css/style-desktop.css\" />
+    <link rel="stylesheet" href="css/skel-noscript.css" />
+    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="css/style-desktop.css" />
   </noscript>
-<link rel=\"stylesheet\" href=\"./node_modules/bootstrap/dist/css/bootstrap.css\" />
-<link rel=\"stylesheet\" href=\"./node_modules/slick-carousel/slick/slick.css\" />
-<link rel=\"stylesheet\" href=\"./node_modules/slick-carousel/slick/slick-theme.css\" />
-<script src=\"./js/vendor/smooth-scroll/smooth-scroll.min.js\"></script>
-<script src=\"./js/services/contenttheme.js\"></script>
-<link rel=\"stylesheet\" href=\"./css/lightbox.css\">
-<link rel=\"stylesheet\" href=\"./css/load-more-button.css\">
-<link rel=\"stylesheet\" href=\"./css/multilinecss.css\">
-<link rel=\"stylesheet\" href=\"https://use.fontawesome.com/releases/v5.0.13/css/all.css\" integrity=\"sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp\" crossorigin=\"anonymous\">
-<!--[if lte IE 8]><link rel=\"stylesheet\" href=\"css/ie/v8.css\" /><![endif]-->
-<!--[if lte IE 9]><link rel=\"stylesheet\" href=\"css/ie/v9.css\" /><![endif]-->
-
-";
-
- ?>
+<link rel="stylesheet" href="./node_modules/bootstrap/dist/css/bootstrap.css" />
+<link rel="stylesheet" href="./node_modules/slick-carousel/slick/slick.css" />
+<link rel="stylesheet" href="./node_modules/slick-carousel/slick/slick-theme.css" />
+<script src="./js/vendor/smooth-scroll/smooth-scroll.min.js"></script>
+<script src="./js/services/contenttheme.js"></script>
+<link rel="stylesheet" href="./css/lightbox.css">
+<link rel="stylesheet" href="./css/load-more-button.css">
+<link rel="stylesheet" href="./css/multilinecss.css">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
